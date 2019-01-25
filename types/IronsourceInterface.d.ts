6ecaab8e93d74452bd956a72090f8d8e95c779f1
mingz <305603665@qq.com>
2019-01-25 20:29:31 +0800

    ts

---
+++ types/IronsourceInterface.d.ts
@@ -0,0 +1,14 @@
+declare class IronsourceInterface {
+
+
+    static init(): void;
+
+    static showBanner(): void;
+
+    static hideBanner(): void;
+
+    static showInterstitial(): void;
+
+    static showRewardedVideo(): void;
+
+}
